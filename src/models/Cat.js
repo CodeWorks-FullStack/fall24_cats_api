b775c8b166b6a7b017965172a6d54aa12ed33f3f
Jeremy Fowler <jembyfowler@gmail.com>
2024-09-09 12:25:39 -0600

    don't forget the maxlength

--- src/models/Cat.js
+++ src/models/Cat.js
@@ -5,6 +5,6 @@ export const CatSchema = new Schema(
     name: { type: String, required: true, maxlength: 100, minlength: 2 },
     numberOfLegs: { type: Number, required: true, min: 0, max: 4, default: 4 },
     likesCheese: { type: Boolean },
-    color: { type: String, required: true }
+    color: { type: String, required: true, maxlength: 25 }
   }
 )
